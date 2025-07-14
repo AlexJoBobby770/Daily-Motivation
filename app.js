4a706f35a037a0d2228f543a6f6bcf15d1db3ff9
alex jo bobby <alexjocoding@gmail.com>
2025-07-14 17:37:15 +0530

    added css and other features

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ function displaySummary() {
   summaryDisplay.innerHTML = `
     <h3>Motivation</h3>
     <p>${text || "—"}</p>
-
     <h3>Goals</h3>
-    <ul>
-      ${goals.map((g, i) => `<li>${g.text || "—"}</li>`).join("")}
+    <ul style="list-style: none;">
+    ${goals.map((g, i) =>
+             `<li >
+            <input type="checkbox" id="goalCheck${i}" ${g.done ? "checked" : ""}>
+            <label for="goalCheck${i}" style="text-decoration: ${g.done ? "line-through" : "none"};">
+            ${g.text}
+            </label>
+        </li>`).join("")}
     </ul>
-
     <h3>Reflection</h3>
     <p>${reflection || "—"}</p>
-    <p>Rating: ${"⭐".repeat(rating)}</p> `;
+    <p>Rating: ${"⭐".repeat(rating)}</p> `
+    goals.forEach((g, i) => {
+  const checkbox = document.getElementById(`goalCheck${i}`);
+  if (checkbox) {
+    checkbox.addEventListener("change", () => {
+      goals[i].done = checkbox.checked;
+      displaySummary();
+    });
+  }
+});
+
 }
 const motivationInput=document.getElementById('motivationInput');
 const addMotivationBtn=document.getElementById('addMotivationBtn');
@@ -29,15 +43,15 @@ const goal1=document.getElementById('goal1');
 const goal2=document.getElementById('goal2');
 const saveGoalsBtn=document.getElementById('saveGoalsBtn');
 
-let goals=[];
+let  goals=[];
 saveGoalsBtn.addEventListener('click',()=>
 {
        goals = [
     { text: goal1.value, done: false },
     { text: goal2.value, done: false },
   ];
-  goal1Input.value = "";
-  goal2Input.value = "";
+  goal1.value = "";
+  goal2.value = "";
   displaySummary();
 });
 
@@ -54,6 +68,47 @@ submitReflectionBtn.addEventListener("click", () => {
   reflectionInput.value = "";
   ratingSelect.value = "1";
   displaySummary();
+});
+
+async function fetchMotivationLogs() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=3");
+    const data = await res.json();
+    console.log("Fetched Motivation Logs:", data);
+  } 
+  catch (error)
+   {
+    console.error("Error fetching logs:", error);
+  }
+}
+fetchMotivationLogs();
+async function postMotivation(text) {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts",
+    {
+      method: "POST",
+      headers: {"Content-Type": "application/json",},
+      body: JSON.stringify({
+        title: "Motivation",
+        body: text,
+        userId: 1,
+      }),
+    });
+
+    const data = await res.json();
+    console.log("Posted Motivation:", data);
+    
+
+  } catch (err) {
+    console.error("Error posting motivation:", err);
+  }
+}
+addMotivationBtn.addEventListener("click", () => 
+        {
+        motivationText = motivationInput.value;
+        postMotivation(motivationText); 
+        motivationInput.value = "";
+        displaySummary();
 });
 
 
